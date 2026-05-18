e5473f3046a8e02711387376c00a71eaeb331fed
re <reiya.0218.macno@icloud.com>
2026-05-18 14:16:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="fastapi-qrcode-generator",
     version="0.1",
-    py_modules=["fastapi-qrcode-generator"],
+    py_modules=["fastapi_qrcode_generator"],
 )
